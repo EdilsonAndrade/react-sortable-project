780acc1d01413bb3d5ebcf2c0be83d746792b7c5
Edilson <edilson.a.andrade@gmail.com>
2021-09-24 07:28:07 -0300

    read me

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,55 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {arrayMove, SortableContainer, SortableElement} from 'react-sortable-hoc';
+
 
 function App() {
+  const [sortableItems, setSortableItems] = useState<string[]>([]);
+
+  useEffect(()=>{
+    const newItems:string[] =[];
+let i =1;
+    while(i<100){
+      console.log('vaii')
+      newItems.push(`Item ${i}`)
+      i++
+    }
+  
+    console.log(newItems, 'entrei')
+    setSortableItems(newItems);
+  },[])
+
+  const SortableItem = SortableElement(({value}:{value:string}) => <div>{value}</div>);
+  const SortableList = SortableContainer(({sortableItems}:{sortableItems:string[]}) => {
+    return (
+      <div style={{position:'relative', 
+      background: '#f3f3f3',  
+      border:'1px solid #efefef', 
+      borderRadius:'3px', 
+      outline:'none',
+      }}>
+        {sortableItems.map((value, index) => {
+          console.log(value, index, 'edilson')
+          return(
+          <SortableItem key={`item-${value}`} index={index} value={value} />
+          )})}
+      </div>
+    );
+  });
+
+
+  const onSortEnd = ({oldIndex, newIndex}:{oldIndex:number, newIndex:number})=>{
+
+    const newposition = arrayMove(sortableItems,oldIndex, newIndex);
+
+    setSortableItems(newposition)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{height:'7150px'}}>
+    <SortableList sortableItems={sortableItems} onSortEnd={onSortEnd} useWindowAsScrollContainer={true} />
     </div>
+   
   );
 }
 
