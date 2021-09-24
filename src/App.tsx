c1bf2f3490d84d658ad835b24cee14e068a798a7
Edilson <edilson.a.andrade@gmail.com>
2021-09-24 11:51:09 -0300

    improving more elements on the page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {arrayMove, SortableContainer, SortableElement} from 'react-sortable-hoc';
-
+import './App.scss';
 
 function App() {
   const [sortableItems, setSortableItems] = useState<string[]>([]);
@@ -23,11 +23,8 @@ let i =1;
   const SortableItem = SortableElement(({value}:{value:string}) => <div>{value}</div>);
   const SortableList = SortableContainer(({sortableItems}:{sortableItems:string[]}) => {
     return (
-      <div style={{position:'relative', 
-      background: '#f3f3f3',  
-      border:'1px solid #efefef', 
-      borderRadius:'3px', 
-      outline:'none',
+      <div style={{ 
+      background: '#f3f3f3'
       }}>
         {sortableItems.map((value, index) => {
           console.log(value, index, 'edilson')
@@ -46,8 +43,31 @@ let i =1;
     setSortableItems(newposition)
   }
   return (
-    <div style={{height:'7150px'}}>
+    <div>
+    <nav className="container">
+    <ul className="container__menu">
+      <li>menu 1</li>  
+      <li>menu 2</li>  
+      <li>menu 3</li>  
+    </ul>  
+    
+    </nav>
+    <div style={{background:'#444', display:"flex", justifyContent:"center", alignItems:"center", color:'#fff', width:'100%', height:'200px', fontSize:'45px'}}>
+      TESTE 
+    </div>
+    <div style={{display:'flex', flexDirection:"row"}}>
+    <aside style={{display:'flex', flexDirection:"column", background:"#ffc"}}>
+          <div>
+            outros elementos
+          </div>
+      </aside>
+    <aside style={{display:'flex', flexDirection:"column"}}>
+    
     <SortableList sortableItems={sortableItems} onSortEnd={onSortEnd} useWindowAsScrollContainer={true} />
+      </aside>  
+    
+
+    </div>
     </div>
    
   );
